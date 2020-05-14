796a34213328b9da9d4808adde407f3dd8a1e6a1
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-05-13 17:31:50 -0700

    switched teamList around

--- src/teamList.js
+++ src/teamList.js
@@ -1,17 +1,17 @@
 const teamList = [
   {
-    name: "Raul",
-    id: "Raul",
-    school: "Lambda School",
-    role: "Student",
-    url: "https://github.com/rauleflores",
-  },
-  {
     name: "Matthew",
     id: "Matthew",
     school: "Lambda School",
     role: "TL/Student",
     url: "https://github.com/Matt-GitHub",
+  },
+  {
+    name: "Raul",
+    id: "Raul",
+    school: "Lambda School",
+    role: "Student",
+    url: "https://github.com/rauleflores",
   },
 ];
 
